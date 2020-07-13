3ba17fbe2169db5051e6cb36bdfd30cf974c780d
Matt Soucoup <masoucou@microsoft.com>
2020-07-13 14:02:31 -0700

    adding xamarin app

---
+++ src/FitApp/FitApp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FitApp.Core
+{
+    public static class Constants
+    {
+        public static readonly string UserIdPreference = "userid";
+
+        public static readonly string DataSyncPointPreference = "syncpoint";
+
+//#error Enter your website name
+        public static readonly string WebServerBaseUrl = "https://fitfitappweb.azurewebsites.net/";
+    }
+}
